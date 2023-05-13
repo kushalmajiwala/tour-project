5cfe79921e6619c096431797ed3490e75f0d8f2b
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-05-13 11:39:50 +0530

    13 may morning work done

--- project_frontend_main/src/main/java/Controllers/UserController.java
+++ project_frontend_main/src/main/java/Controllers/UserController.java
@@ -1,10 +1,17 @@
 package Controllers;
 
 import UserEJB.UserBeanLocal;
+import entity.Tourmaster;
+import entity.Tourplace;
 import entity.Usertb;
+import entity.Vehicle;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.http.HttpSession;
 import org.glassfish.soteria.identitystores.hash.Pbkdf2PasswordHashImpl;
@@ -27,7 +34,7 @@ public class UserController implements Serializable {
         System.out.println("Session Destroyed...");
         return "gallery.xhtml?faces-redirect=true";
     }
-    
+
     //Edit Profile Work
     Usertb user_info = new Usertb();
 
@@ -202,13 +209,215 @@ public class UserController implements Serializable {
     public String redirectToAdminHome() {
         return "userHome.xhtml?faces-redirect=true";
     }
-    
-    public String redirectAllTours()
-    {
+
+    public String redirectAllTours() {
         return "userHome.xhtml?faces-redirect=true";
     }
-    public void goTofeedback()
-    {
-    
+
+    public List<Tourmaster> getAllTours() {
+        return ubl.showMasterData();
+    }
+    //Day difference
+    String startDate;
+    String endDate;
+    int time_diff;
+    int day_diff;
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getTime_diff() {
+        return time_diff;
+    }
+
+    public void setTime_diff(int time_diff) {
+        this.time_diff = time_diff;
+    }
+
+    public int getDay_diff() {
+        return day_diff;
+    }
+
+    public void setDay_diff(int day_diff) {
+        this.day_diff = day_diff;
+    }
+
+    public int getDayDiff(String start_date, String end_date) {
+        startDate = start_date;
+        endDate = end_date;
+
+        SimpleDateFormat obj = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date sdate = obj.parse(startDate);
+            Date edate = obj.parse(endDate);
+
+            time_diff = (int) (edate.getTime() - sdate.getTime());
+            day_diff = (time_diff / (1000 * 60 * 60 * 24)) % 365;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return day_diff;
+    }
+    //showUserTemplate
+    String sDate;
+    String eDate;
+    String tourTitle;
+    int tourPrice;
+    int times_diff;
+    int days_diff;
+    int data_length;
+    int day_num;
+    String picUrl;
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getsDate() {
+        return sDate;
+    }
+
+    public void setsDate(String sDate) {
+        this.sDate = sDate;
+    }
+
+    public String geteDate() {
+        return eDate;
+    }
+
+    public void seteDate(String eDate) {
+        this.eDate = eDate;
+    }
+
+    public int getTimes_diff() {
+        return times_diff;
+    }
+
+    public void setTimes_diff(int times_diff) {
+        this.times_diff = times_diff;
+    }
+
+    int tourmasterid;
+
+    public int getTourmasterid() {
+        return tourmasterid;
+    }
+
+    public void setTourmasterid(int tourmasterid) {
+        this.tourmasterid = tourmasterid;
+    }
+
+    public int getDay_num() {
+        return day_num;
+    }
+
+    public void setDay_num(int day_num) {
+        this.day_num = day_num;
+    }
+
+    public int getData_length() {
+        return data_length;
+    }
+
+    public void setData_length(int data_length) {
+        this.data_length = data_length;
+    }
+
+    public int getDays_diff() {
+        return days_diff;
+    }
+
+    public void setDays_diff(int days_diff) {
+        this.days_diff = days_diff;
+    }
+
+    public int getTourPrice() {
+        return tourPrice;
+    }
+
+    public void setTourPrice(int tourPrice) {
+        this.tourPrice = tourPrice;
+    }
+
+    public String getTourTitle() {
+        return tourTitle;
+    }
+
+    public void setTourTitle(String tourTitle) {
+        this.tourTitle = tourTitle;
+    }
+
+    List<String> temp = new ArrayList<>();
+
+    public List<String> tempFunc() {
+        temp = new ArrayList<>();
+        temp.add("Surat");
+        return temp;
+    }
+
+    List<Tourplace> all_places;
+
+    public String populate_places(int tourplaceid, String pic_url, String start_date, String end_date, String tour_title, int tour_price) {
+        tourmasterid = tourplaceid;
+        sDate = start_date;
+        eDate = end_date;
+        picUrl = pic_url;
+        tourTitle = tour_title;
+        tourPrice = tour_price;
+        day_num = 0;
+        SimpleDateFormat obj = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            Date sdate = obj.parse(sDate);
+            Date edate = obj.parse(eDate);
+
+            times_diff = (int) (edate.getTime() - sdate.getTime());
+            days_diff = (times_diff / (1000 * 60 * 60 * 24)) % 365;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Done");
+        all_places = new ArrayList<>();
+        all_places = ubl.getPlaces(tourplaceid);
+        data_length = all_places.size();
+
+        System.out.println("All Tours -> " + all_places);
+        System.out.println("I am done Here");
+
+        return "showUserTemplate.xhtml?faces-redirect=true";
+    }
+
+    public List<Tourplace> getAllPlaces() {
+        System.out.println("Getting Places -> " + all_places);
+        return all_places;
+    }
+
+    public List<Vehicle> getAllVehicleByTourmasterid() {
+        System.out.println("Getting Vehicle");
+        List<Vehicle> all_vehicle = ubl.getVehicle(tourmasterid);
+        System.out.println("All Vehicle -> " + all_vehicle);
+        return all_vehicle;
+    }
+
+    public String redirectToUserHome() {
+        return "userHome.xhtml?faces-redirect=true";
     }
 }
